ebb95adcf86afae605efe336888111bfbabeba5f
chicongn <chicongvn1205@gmail.com>
2025-08-29 13:23:35 +0700

    update: add draw snake function

--- snake/script.js
+++ snake/script.js
@@ -7,3 +7,18 @@ const ROWS = canvas.height / BOX;
 
 ctx.fillStyle = 'red';
 ctx.fillRect(0, 0, BOX, BOX);
+
+let snake = [
+  {x: 9 * BOX, y: 9 * BOX},
+  {x: 8 * BOX, y: 9 * BOX},
+  {x: 7 * BOX, y: 9 * BOX}
+];
+
+function drawSnake() {
+  for (let i = 0; i < snake.length; i++) {
+    ctx.fillStyle = i === 0 ? 'lime' : 'white';
+    ctx.fillRect(snake[i].x, snake[i].y, BOX, BOX);
+  }
+}
+
+drawSnake();
